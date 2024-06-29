88ff86bce4316165b150fe84d5b0ac5e0b765244
Ruth Georgiev <ruth.georgiev@proton.me>
2024-06-29 16:44:59 +0200

    fix deployment - modified vide.config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react-todo.list/', // Ensure this matches your repo name
+  base: '/react-todo.list/', // Ensure this matches your repository name
   build: {
-    outDir: 'dist', // Ensure this matches your deploy folder
-  }
+    outDir: 'dist',
+  },
 });
